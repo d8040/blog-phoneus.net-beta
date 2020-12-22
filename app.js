d7203ab0ad8f4ce7d6f0fd4c21409e187d9227bb
d8040 <d8040@nate.com>
2020-12-23 01:16:44 +0900

    반응형앱 적용

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('.side-menu');
+var lineOne = document.querySelector('.side-menu .menu-btn .line--1');
+var lineTwo = document.querySelector('.side-menu .menu-btn .line--2');
+var lineThree = document.querySelector('.side-menu .menu-btn .line--3');
+var link = document.querySelector('.side-menu .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
 console.clear();
 
 ScrollOut({
